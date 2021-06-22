467bf3925fb8c7de9d3f20d83fa174624af05f24
khalidhmd <khalidhmd@outlook.com>
2021-06-22 22:03:19 +0200

    copy file from cache to data folder

--- src/pages/Tab2.jsx
+++ src/pages/Tab2.jsx
@@ -16,70 +16,13 @@ import {
   IonButton,
 } from "@ionic/react";
 import { useState, useEffect } from "react";
-import { isPlatform } from "@ionic/react";
-import {
-  Camera,
-  CameraResultType,
-  CameraSource,
-  Photo,
-} from "@capacitor/camera";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Storage } from "@capacitor/storage";
 import { Capacitor } from "@capacitor/core";
 import "./Tab2.css";
 
 const PHOTO_STORAGE = "photos";
-
-async function base64FromPath(path) {
-  const response = await fetch(path);
-  const blob = await response.blob();
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = () => {
-      if (typeof reader.result === "string") {
-        resolve(reader.result);
-      } else {
-        reject("method did not return a string");
-      }
-    };
-    reader.readAsDataURL(blob);
-  });
-}
-
-const savePicture = async (cameraPhoto, fileName) => {
-  let base64Data;
-  // "hybrid" will detect Cordova or Capacitor;
-  if (isPlatform("hybrid")) {
-    const file = await Filesystem.readFile({
-      path: cameraPhoto.path,
-    });
-    base64Data = file.data;
-  } else {
-    base64Data = await base64FromPath(cameraPhoto.webPath);
-  }
-  const savedFile = await Filesystem.writeFile({
-    path: fileName,
-    data: base64Data,
-    directory: Directory.Data,
-  });
-
-  if (isPlatform("hybrid")) {
-    // Display the new image by rewriting the 'file://' path to HTTP
-    // Details: https://ionicframework.com/docs/building/webview#file-protocol
-    return {
-      filepath: savedFile.uri,
-      webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-    };
-  } else {
-    // Use webPath to display the new image instead of base64 since it's
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath,
-    };
-  }
-};
 
 const takePhoto = async () => {
   const cameraPhoto = await Camera.getPhoto({
@@ -89,8 +32,21 @@ const takePhoto = async () => {
     presentationStyle: "popover",
   });
   const fileName = new Date().getTime() + "." + cameraPhoto.format;
-  const takenPhoto = await savePicture(cameraPhoto, fileName);
-  return takenPhoto;
+
+  Filesystem.copy({
+    from: cameraPhoto.path,
+    to: fileName,
+    toDirectory: Directory.Data,
+  });
+
+  const finalPhotoUri = await Filesystem.getUri({
+    directory: Directory.Data,
+    path: fileName,
+  });
+
+  let webviewPath = Capacitor.convertFileSrc(finalPhotoUri.uri);
+
+  return { webviewPath };
 };
 
 const Tab2 = () => {
@@ -101,17 +57,7 @@ const Tab2 = () => {
     const loadSaved = async () => {
       const { value } = await Storage.get({ key: PHOTO_STORAGE });
       const photosInStorage = value ? JSON.parse(value) : [];
-
-      if (!isPlatform("hybrid")) {
-        for (let photo of photosInStorage) {
-          const file = await Filesystem.readFile({
-            path: photo.filepath,
-            directory: Directory.Data,
-          });
-          // Web platform only: Load the photo as base64 data
-          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
-        }
-      }
+      console.log(photosInStorage);
       setPhotos(photosInStorage);
     };
     loadSaved();
@@ -130,13 +76,15 @@ const Tab2 = () => {
 
   const handleDeletePhoto = async (photo) => {
     // Remove this photo from the Photos reference data array
-    const newPhotos = photos.filter((p) => p.filepath !== photo.filepath);
+    const newPhotos = photos.filter((p) => p.webviewPath !== photo.webviewPath);
 
     // Update photos array cache by overwriting the existing photo array
     Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
 
     // delete photo file from filesystem
-    const filename = photo.filepath.substr(photo.filepath.lastIndexOf("/") + 1);
+    const filename = photo.webviewPath.substr(
+      photo.webviewPath.lastIndexOf("/") + 1
+    );
     await Filesystem.deleteFile({
       path: filename,
       directory: Directory.Data,
